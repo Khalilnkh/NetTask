6f6384d7d1c385551e55c07a998146ef8b32ba31
“Khalilnkhgit config --global user.email “khalilnkh@code.edu.az <khalilnkh@code.edu.az>
2024-01-22 05:12:11 +0900

    Created

---
+++ Multiplelayouta/ASP.NET_Partial/ASP.NET_Partial/Program.cs
@@ -0,0 +1,19 @@
+namespace ASP.NET_Partial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddRazorPages();
+            var app = builder.Build();
+
+            app.UseRouting();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
